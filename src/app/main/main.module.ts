449bfb3bfc289ac59be6b99101634ad2b3ce10a1
aptivator <aptivator@gmail.com>
2017-08-03 15:37:16 -0400

    Add initial files

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,16 @@
+import {NgModule}            from '@angular/core'; 
+import {BrowserModule}       from '@angular/platform-browser';
+import {MainComponent}       from './main/main.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    MainComponent
+  ],
+  bootstrap: [
+    MainComponent
+  ]
+})
+export class MainModule {}
---
+++ src/app/main/main/main.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'main-app',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.less']
+})
+export class MainComponent {}
